ea09149a7e92e757d8b2b4fc3389437eccc40ae1
lawrencejews <lawrencejews1@gmail.com>
2022-01-27 11:16:19 +0900

    intervals and timers for subscription

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -3,3 +3,14 @@ import { setCount, startButton, pauseButton } from './utilities';
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
+
+let interval$ = interval(1000);
+let subscription
+
+start$.subscribe(() => {
+  subscription = interval$.subscribe(setCount);
+});
+
+pause$.subscribe(() => {
+  subscription.unsubscribe();
+});
